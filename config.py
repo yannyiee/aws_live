353c69375debdc3c3ede919fe598e268e0286747
yannyiee <100904890+yannyiee@users.noreply.github.com>
2023-04-11 08:47:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cqobzg2r7v7a.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "wongyannyiee_employee"
+custombucket = "wongyannyiee-employee"
+customregion = "us-east-1"
